3f3b697bf890951f5cac85fbaa3fa1585be4e06d
ghostlypi <parth.iyer@gmail.com>
2017-06-11 17:31:28 -0700

    Create stuff.py

--- stuff.py
+++ stuff.py
@@ -1,3 +1,16 @@
-from turtle import *
+def encrypt(user_input):
+    myname = user_input
+    bigstring = ""
+    for x in range (len(myname)):
+        bigstring = bigstring + str(ord(myname[x])+3).zfill(3)
 
-fd(100)
+    x = 0
+    encrypted = ""
+    while x < len(bigstring):
+        charnumber = bigstring[x:x+3]
+        x = x + 3
+        charnumber = int(charnumber)
+        encrypted = encrypted + chr(charnumber)
+
+
+    return encrypted
